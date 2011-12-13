4e7b12f89e84654e474554211ef049edd2c7a9e2
kevin19900306 <kevin19900306@9468747c-ddcf-3e0b-eead-2f29c2eda07a>
2011-12-13 06:49:27 +0000

    First commit

---
+++ trunk/Android/GuessNumber/src/com/ncs/hj/GuessNumberActivity.java
@@ -0,0 +1,13 @@
+package com.ncs.hj;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GuessNumberActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
